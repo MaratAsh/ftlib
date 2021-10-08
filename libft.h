718497fa0e2b5418ebcd06982f6059ae489264fb
Altagracia Cierra <alcierra@va-a3.kzn.21-school.ru>
2021-10-08 22:30:05 +0300

    libft.h was updated

--- libft.h
+++ libft.h
@@ -6,12 +6,14 @@
 /*   By: alcierra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/04 12:00:00 by alcierra          #+#    #+#             */
-/*   Updated: 2021/10/06 13:34:35 by alcierra         ###   ########.fr       */
+/*   Updated: 2021/10/08 21:54:53 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+
+# include <stdlib.h>
 
 typedef unsigned long	t_size;
 
@@ -25,8 +27,8 @@ void	*ft_memset(void *b, int c, t_size len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, t_size n);
 void	*ft_memmove(void *dst, const void *src, t_size len);
-t_size	ft_strlcpy(char *dst, const char *restrict src, t_size sz);
-t_size	ft_strlcat(char *dst, const char *restrict src, t_size sz);
+t_size	ft_strlcpy(char *dst, const char *src, t_size sz);
+t_size	ft_strlcat(char *dst, const char *src, t_size sz);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
@@ -38,5 +40,20 @@ int		ft_atoi(const char *str);
 
 void	*ft_calloc(size_t count, t_size size);
 char	*ft_strdup(const char *s1);
+
+char	*ft_substr(char const *s, unsigned int start,size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+char	*ft_strtrim(char const *s1, char const *set);
+
+char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
