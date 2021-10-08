1c231afe2e7dfe217eda9b2dd95e2c06788aa059
Altagracia Cierra <alcierra@va-a3.kzn.21-school.ru>
2021-10-08 22:29:41 +0300

    ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,13 +6,26 @@
 /*   By: alcierra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 16:53:10 by alcierra          #+#    #+#             */
-/*   Updated: 2021/10/08 16:54:07 by alcierra         ###   ########.fr       */
+/*   Updated: 2021/10/08 21:39:26 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsignedint, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+	char			*str;
+	unsigned int	i;
+
+	str = (char *) malloc(sizeof(char) * (ft_strlen((char *) s) + 1));
+	if (str == NULL)
+		return (str);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	str[i] = 0;
+	return (str);
 }
