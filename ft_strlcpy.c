207947025f52a5f91418c3d66415d93a8a0bca4f
Altagracia Cierra <alcierra@student.21-school.ru>
2021-10-09 14:09:47 +0300

    ft_strlcpy.c norm

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,25 +6,26 @@
 /*   By: alcierra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 16:04:22 by alcierra          #+#    #+#             */
-/*   Updated: 2021/10/08 15:25:58 by alcierra         ###   ########.fr       */
+/*   Updated: 2021/10/09 14:09:22 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef unsigned long	t_size;
+#include "libft.h"
 
 t_size	ft_strlcpy(char *dst, const char *src, t_size sz)
 {
 	t_size	i;
 
 	i = 0;
+	if (sz == 0)
+		return (ft_strlen((char *) src));
 	while (src[i])
 	{
-		if (i == sz)
-			break;
+		if (i == sz - 1)
+			break ;
 		dst[i] = src[i];
 		i++;
 	}
-	if ((i > 0 && i < sz) || (src[i] == 0 && i < sz))
-		dst[i] = 0;
+	dst[i] = 0;
 	return (i);
 }
