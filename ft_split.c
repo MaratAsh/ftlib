7f70dad4745a0f7cb9b83adeaf3df8debd8716ee
Altagracia Cierra <alcierra@student.21-school.ru>
2021-10-10 13:17:53 +0300

    ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: alcierra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 16:50:34 by alcierra          #+#    #+#             */
-/*   Updated: 2021/10/08 19:50:13 by alcierra         ###   ########.fr       */
+/*   Updated: 2021/10/10 13:16:27 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,39 +46,48 @@ static int	ft_splited_cnt(char const *s, char c)
 	return (count);
 }
 
+static int	ft_split_free(char **arr_str, t_size word_cnt)
+{
+	while (word_cnt--)
+		free(arr_str[word_cnt]);
+	free(arr_str);
+	return (0);
+}
+
+static int	ft_split_part(char const *s, char c, char **arr_str)
+{
+	t_size	word_len;
+	t_size	dsp;
+	t_size	word_cnt;
+
+	dsp = 0;
+	word_cnt = 0;
+	while (s[dsp] == c && c)
+		dsp++;
+	while (s[dsp])
+	{
+		word_len = ft_size_part((char *) s + dsp, c);
+		arr_str[word_cnt] = malloc(sizeof(char) * (word_len + 1));
+		if (arr_str[word_cnt] == NULL)
+			return (ft_split_free(arr_str, word_cnt));
+		ft_strlcpy(arr_str[word_cnt], s + dsp, word_len + 1);
+		dsp += word_len;
+		while (*(s + dsp) == c)
+			++dsp;
+		++word_cnt;
+	}
+	arr_str[word_cnt] = NULL;
+	return (1);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**arr_str;
-	int		i;
-	int		count_word;
-	t_size	word_len;
-	t_size	displmnt;
-	t_size	str_len;
 
-	count_word = ft_splited_cnt(s, c);
-	arr_str = (char **) malloc(sizeof(char *) * (count_word + 1));
+	arr_str = (char **) malloc(sizeof(char *) * (ft_splited_cnt(s, c) + 1));
 	if (arr_str == (char **) 0)
 		return (NULL);
-	str_len = ft_strlen((char *) s);
-	displmnt = 0;
-	i = 0;
-	while (i < count_word)
-	{
-		word_len = ft_size_part((char *) s + displmnt, c);
-		arr_str[i] = malloc(sizeof(char) * (word_len + 1));
-		if (arr_str[i] == NULL)
-		{
-			while(i--)
-				free(arr_str[i]);
-			free(arr_str);
-			return (NULL);
-		}
-		ft_strlcpy(arr_str[i], s + displmnt, word_len + 1);
-		displmnt += word_len + 1;
-		while (*(s + displmnt) == c)
-			displmnt++;
-		i++;
-	}
-	arr_str[i] = NULL;
+	if (ft_split_part(s, c, arr_str) == 0)
+		return (NULL);
 	return (arr_str);
 }
