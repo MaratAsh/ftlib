5873d80fcfa15ac7d34b582e0d2365924c983324
Altagracia Cierra <alcierra@student.21-school.ru>
2021-10-10 15:44:49 +0300

    ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: alcierra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 17:07:16 by alcierra          #+#    #+#             */
-/*   Updated: 2021/10/08 13:54:43 by alcierra         ###   ########.fr       */
+/*   Updated: 2021/10/10 15:44:21 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@ typedef unsigned long	t_size;
 
 int	ft_strncmp(const char *str1, const char *str2, t_size n)
 {
-	t_size	i;
+	t_size			i;
 	unsigned char	*s1;
 	unsigned char	*s2;
 
