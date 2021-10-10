e7a16cd43f2fac3a3570820081a6c2e1023352f4
Altagracia Cierra <alcierra@student.21-school.ru>
2021-10-10 13:34:01 +0300

    ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: alcierra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 16:22:04 by alcierra          #+#    #+#             */
-/*   Updated: 2021/10/08 16:37:20 by alcierra         ###   ########.fr       */
+/*   Updated: 2021/10/10 13:32:43 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	len1 = ft_strlen((char *) s1);
 	len2 = ft_strlen((char *) s2);
 	str = (char *) malloc(sizeof(char) * (len1 + len2 + 1));
-	if (str == (char *) 0)
+	if (str == NULL)
 		return (NULL);
-	ft_strlcpy(str, s1, len1);
+	ft_strlcpy(str, s1, len1 + 1);
 	ft_strlcpy(str + len1, s2, len2 + 1);
 	return (str);
 }
