67e55e83e38664b463ff5e569205b2d7d66c5597
Altagracia Cierra <alcierra@student.21-school.ru>
2021-10-10 16:14:26 +0300

    ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: alcierra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 17:38:54 by alcierra          #+#    #+#             */
-/*   Updated: 2021/10/08 13:35:17 by alcierra         ###   ########.fr       */
+/*   Updated: 2021/10/10 16:05:20 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,11 @@ int	ft_memcmp(const void *s1, const void *s2, t_size n)
 	s1_ = (unsigned char *) s1;
 	s2_ = (unsigned char *) s2;
 	i = 0;
+	if (s1_ == 0 && s2_ == 0)
+		return (0);
 	while (s1_[i] == s2_[i] && i < n)
 		i++;
+	if (i >= n)
+		return (s1_[i - 1] - s2_[i - 1]);
 	return (s1_[i] - s2_[i]);
 }
