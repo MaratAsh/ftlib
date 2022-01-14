2848d1dfe48f2e78baa7cab9fed9ed9bedf181f7
Altagracia Cierra <alcierra@un-c5.kzn.21-school.ru>
2022-01-14 18:36:41 +0300

    optimizing

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: alcierra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/08 16:56:22 by alcierra          #+#    #+#             */
-/*   Updated: 2021/10/10 16:09:39 by alcierra         ###   ########.fr       */
+/*   Updated: 2022/01/14 18:35:14 by alcierra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,7 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	unsigned long	i;
-
-	i = 0;
 	if (!s)
 		return ;
-	while (s[i])
-	{
-		write(fd, s + i, 1);
-		i++;
-	}
+	write(fd, s, ft_strlen(s));
 }
